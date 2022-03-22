da99ce515cc10a3173980de7f4ce88ced39929a7
codriv <combodriver4@yahoo.at>
2022-03-22 17:49:15 +0100

    JurassicPark - refactor

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/jurassic/JurassicPark.java
@@ -18,16 +18,20 @@ public class JurassicPark {
     }
 
     public List<String> checkOverpopulation() {
-        List<String> overPopulation = new ArrayList<>();
         try(Connection con = dataSource.getConnection();
             Statement ps = con.createStatement();
             ResultSet rs = ps.executeQuery("select breed from dinosaur where actual > expected order by breed")) {
-            while (rs.next()) {
-                String breed = rs.getString("breed");
-                overPopulation.add(breed);
-            }
+            return getPopulation(rs);
         } catch (SQLException sqle) {
             throw new IllegalStateException("Cannot reach database!");
+        }
+    }
+
+    private List<String> getPopulation(ResultSet rs) throws SQLException {
+        List<String> overPopulation = new ArrayList<>();
+        while (rs.next()) {
+            String breed = rs.getString("breed");
+            overPopulation.add(breed);
         }
         return overPopulation;
     }
